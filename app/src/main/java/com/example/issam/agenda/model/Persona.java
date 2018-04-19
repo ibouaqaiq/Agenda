dd869011c674a8fee6b6c81f6e34895662478fcb
issamPortatil <ibouaqaiq@gmail.com>
2018-04-19 16:31:02 +0200

    nuew

---
+++ app/src/main/java/com/example/issam/agenda/model/Persona.java
@@ -0,0 +1,33 @@
+package com.example.issam.agenda.model;
+
+public class Persona {
+    private String nom;
+    private String cognoms;
+    private int edad;
+
+
+    public Persona(String nomPersona, String cognomsPersona, int edadPersona) {
+        this.nom = nomPersona;
+        this.cognoms = cognomsPersona;
+        this.edad = edadPersona;
+    }
+
+    public void posarNom(String valornom) {
+        nom = valornom;
+    }
+    public void posarCognoms(String valorcognoms) {
+        cognoms = valorcognoms;
+    }
+    public void posarEdad (int valoredad){edad = valoredad; }
+
+    public String obtenirNom(){
+        return nom;
+    }
+    public String obtenirCognoms(){
+        return cognoms;
+    }
+
+    public int obtenirEdad(){
+        return edad;
+    }
+}

d2abf07941e5910dd62574c359ef6b17931928fa
roger <rmocholisegura@elpuig.xeill.net>
2018-04-26 15:52:47 +0200

    adf

---
+++ app/src/main/java/com/example/issam/agenda/view/editUserActivity.java
@@ -0,0 +1,64 @@
+package com.example.issam.agenda.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.issam.agenda.R;
+import com.example.issam.agenda.model.Persona;
+
+import io.realm.Realm;
+
+public class editUserActivity extends AppCompatActivity {
+    EditText addnombre, addapellido, addedad;
+    Button addfinish;
+
+    Realm realm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+
+        Intent intent = getIntent();
+
+        final Persona persona = intent.getParcelableExtra("persona");
+
+        realm = Realm.getDefaultInstance();
+
+        addnombre = findViewById(R.id.addnombre);
+        addapellido = findViewById(R.id.addapellido);
+        addedad = findViewById(R.id.addedad);
+        addfinish = findViewById(R.id.addfinish);
+
+
+        addnombre.setText(persona.getNom());
+        addapellido.setText(persona.getCognoms());
+        addedad.setText(persona.getEdad());
+
+
+
+        addfinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                persona.setNom(addnombre.getText().toString());
+                persona.setCognoms(addapellido.getText().toString());
+                persona.setEdad(addedad.getText().toString());
+
+                realm.beginTransaction();
+                realm.copyToRealmOrUpdate(persona);
+                realm.commitTransaction();
+
+
+                onBackPressed();
+
+
+            }
+        });
+    }
+}
